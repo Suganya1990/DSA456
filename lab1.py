8799fbdc81d3a0759070eb6a096aae2144a37b57
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-05-24 20:55:14 -0400

    added test file

--- lab1.py
+++ lab1.py
@@ -1,10 +1,9 @@
-#Function 1
+
 def wins_rock_scissors_paper(plGuess, opGuess):
     #converts string into number
     p = convertOption(plGuess)
     o = convertOption(opGuess)
 
-    #compares numbers to determine winner 1 = rock | 2 = paper | 3 = scissors | -1 any other value
     if ( p == o   or p ==-1 or o ==-1 ):
         return False
     if(p ==1 and o ==3 ) or (p==2 and o ==1 ) or (p==3 and o ==2 ):
@@ -24,23 +23,6 @@ def convertOption(option):
         case _:
             return -1
 
-# print ('Test 1 P1:"Rock" P2:"Rock" : ', wins_rock_scissors_paper('Rock', 'rock'),  "|", 'passed' if wins_rock_scissors_paper('Rock', 'rock')==False else 'Failed')#False
-# print ('Test 2 P1:"Rock" P2:"Paper" : ', wins_rock_scissors_paper('Rock', 'Paper'),  "|", 'passed' if wins_rock_scissors_paper('Rock', 'Paper')==False else 'Failed')   #False
-# print ('Test 3 P1:"Rock" P2:"Scissors" : ', wins_rock_scissors_paper('Rock', 'scissors'),"|",  'passed' if wins_rock_scissors_paper('Rock', 'scissors')==True else 'Failed ') #True
-# print ('Test 4 P1:"Paper" P2:"PAPer" : ',wins_rock_scissors_paper('Paper', 'PAPer'), "|", 'passed' if wins_rock_scissors_paper('Paper', 'PAPer')==False else 'Failed')#False
-# print ('Test 5 P1:"Paper" P2:"Rock" : ',wins_rock_scissors_paper('Paper', 'Rock'), "|", 'passed' if wins_rock_scissors_paper('Paper', 'Rock')==True else 'Failed') #True
-# print ('Test 6 P1:"Paper" P2:"Scissors" : ',  wins_rock_scissors_paper('Paper', 'Scissors'), "|",  'passed' if  wins_rock_scissors_paper('Paper', 'Scissors')==False else 'Failed' ) #False
-# print ('Test 7 P1:"Scissors" P2:"Scissors" : ',  wins_rock_scissors_paper('Scissors', 'Scissors'),  "|", 'passed' if wins_rock_scissors_paper('Scissors', 'Scissors')==False else 'Failed')#False
-# print ('Test 8 P1:"Scissors" P2:"Paper" : ',  wins_rock_scissors_paper('Scissors', 'Paper'),  "|", 'passed' if  wins_rock_scissors_paper('Scissors', 'Paper')==True else 'Failed') #True
-# print ('Test 9 P1:"Scissors" P2:"Rock" : ', wins_rock_scissors_paper('Scissors', 'Rock'), "|",  'passed' if wins_rock_scissors_paper('Scissors', 'Rock')==False else 'Failed') #False
-# print ('Test 10 P1:"Scissors" P2:"Rock" : ',wins_rock_scissors_paper('sdfsd', 'Rocdsdsk'), "|",   'passed' if wins_rock_scissors_paper('sdfsd', 'Rocdsdsk')==False else 'Failed') #False
-
-
-
-#************************************************#
-
-# #Function 2 
-# This function passes a non-negative integer, which we will call n in this description. the function returns n! (pronounced n factorial). n! = n * (n-1) * (n-2)... * 1 Thus, 3! = 3 * 2 * 1. Note that 0! is 1 by definition.
 
 def factorial(num):
     i = num-1
@@ -58,37 +40,14 @@ def factorial(num):
 
 
 
-# factorial(5)
-# factorial(6)
-# factorial(7)
-# factorial(8)
-# factorial(9)
-# factorial(20)
 
 
 
 
 
 
-#************************************************#
 
-#Function 3 
-
-# def Fibonacci(num):
-#     n = 0
-#     numArray = []; 
-#     fibonaciiVal= 0  
-#     if(num < 0):
-#         return 
-#     if(num == 0 ):
-#         return 0
-#     else:
-#         while (n >= 0 and n < num   ):
-#             numArray.push()
-#             n=n+1
-
-
-def Fibonacci(num):
+def fibonacci(num):
     for n in range(num):
         total = fibRecursion(n)
         if n == num-1:
@@ -106,16 +65,24 @@ def fibRecursion(n):
 
 
 
-Fibonacci(5)
-Fibonacci(10)
-Fibonacci(35)
-#Fibonacci(100)
 
-
-
-
-
-
-#************************************************#
 
 #Function 4cd 
+class UpCounter:
+    def __init__(self, step = 1):
+        self.Counter  = 0 
+        self.step = step 
+
+    def count(self):
+        return self.Counter
+    def update(self):
+        tempCounter = self.Counter + self.step
+        self.Counter =tempCounter
+    
+class DownCounter(UpCounter):
+    def __init__(self, step=1):
+        super().__init__(step)
+
+    def update(self):
+         tempCounter = self.Counter - self.step
+         self.Counter =tempCounter
